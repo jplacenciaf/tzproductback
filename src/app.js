72b28569261ad936e4e8bfda843ebab531c3ce18
Edwyn <edwynjose@outlook.com>
2023-06-07 21:11:17 -0500

    Add CROSS

--- src/app.js
+++ src/app.js
@@ -14,19 +14,19 @@ app.use((req, res, next) => {
     logger(`Tiempo de respuesta: ${duration}ms`);
   });
 
-    // // Website you wish to allow to connect
-    // // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-    // res.setHeader('Access-Control-Allow-Origin', 'https://productfront.netlify.app');
+    // Website you wish to allow to connect
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Origin', 'https://productfront.netlify.app');
 
-    // // Request methods you wish to allow
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // // Set to true if you need the website to include cookies in the requests sent
-    // // to the API (e.g. in case you use sessions)
-    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
   next();
 });
